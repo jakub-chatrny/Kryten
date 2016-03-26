c93dbbf197f2c6d0d12be51740cbc84ee51316aa
jchatrny <jakub.chatrny@gmail.com>
2016-03-26 15:10:07 +0100

    Drawing a curve using De Castelo.

--- Kryten/src/main/java/kryten/engine/Draw.java
+++ Kryten/src/main/java/kryten/engine/Draw.java
@@ -2,6 +2,9 @@ package kryten.engine;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.awt.Point;
+import java.util.List;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
@@ -45,6 +48,56 @@ public class Draw {
 		glPopMatrix();
 		incDrawCounter();		
 	}
+	
+	/**
+	 * Draws bezier curve with color and texture. Using de castelo algorithm
+	 * @param cntrlPts - control points of curve
+	 * @param tstep - step of iteration 0.0001 is recomened, less better
+	 * @param color - color of curve
+	 * @param texture - texture of curve
+	 */
+	public static void curve(final List<Point> cntrlPts, double tstep, 
+			final Color color, final Texture texture) {
+		Point prev_pt = cntrlPts.get(0);
+		Point pt = cntrlPts.get(0);
+		//for(int i = 0; i < cntrlPts.size()-1; i++){
+		//	if(i != 0) {break;}
+			for(double t = 0; t < 1f; t += tstep){
+				//pt.x = (float) (((1-t) * cntrlPts.get(i).getX()) + t * cntrlPts.get(i+1).getX());
+				//pt.y = y = (float) (((1-t) * cntrlPts.get(i).getY()) + t * cntrlPts.get(i+1).getY());
+				prev_pt = pt;
+				pt = getCasteljauPoint(cntrlPts, cntrlPts.size()-1 ,0,t);
+				glPushMatrix();
+				{
+					glColor4f(color.r, color.g, color.b, color.a);
+	
+					texture.bind();
+					
+				   glPointSize(1.0f);
+				   glBegin(GL_LINES);{
+				         glVertex2f((float)prev_pt.getX(),(float)prev_pt.getY());
+				         glVertex2f((float)pt.getX(),(float)pt.getY());
+				   }
+				   glEnd();
+				}
+				glPopMatrix();
+				incDrawCounter();
+				
+			}
+		//}
+		
+	}
+	
+    private static Point getCasteljauPoint(final List<Point> cntrlPts, int r, int i, double t) { 
+        if(r == 0) return cntrlPts.get(i);
+
+        Point p1 = getCasteljauPoint(cntrlPts, r - 1, i, t);
+        Point p2 = getCasteljauPoint(cntrlPts, r - 1, i + 1, t);
+
+        return new Point((int) ((1 - t) * p1.getX() + t * p2.getX()), (int) ((1 - t) * p1.getY() + t * p2.getY()));
+    }
+    
+
 	/**
 	 * Draws rectangle with color and texture. Draw from x, y coordinates.
 	 * 
@@ -177,4 +230,15 @@ public class Draw {
 	public static int getDrawCounter() {
 		return Draw.drawCounter;
 	}
+	
+	private static long recfact(long start, float k) {
+	    long i, r = 0;
+	    if (k <= 16) { 
+	        for (i = start + 1; i < start + k; i++) r *= i;
+	        return r;
+	    }
+	    i = (long) (k / 2);
+	    return recfact(start, i) * recfact(start + i, k - i);
+	}
+	protected static long factorial(float k) { return recfact(1, k); }
 }
--- Kryten/src/main/java/kryten/example/HelloWorld.java
+++ Kryten/src/main/java/kryten/example/HelloWorld.java
@@ -1,6 +1,9 @@
 package kryten.example;
 
 import java.awt.Font;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.TrueTypeFont;
@@ -39,10 +42,21 @@ public class HelloWorld extends GameTemplate {
 		final int height = 100;
 		final Color color = Color.gray;
 		
-		
+		//drawing a gray rectangle
 		Draw.rect(posX, posY, width, height, color, TextureFactory.get("blank"));
+		
+		//drawing a black line across rectangle
 		Draw.line(posX, posY, posX+width, posY+height, Color.black, TextureFactory.get("blank"));
 		
+		//draw curve
+		final List<Point> controlPoints = new ArrayList<Point>();	
+		controlPoints.add(new Point(posX,posY));		
+		controlPoints.add(new Point(posX,posY+height));
+		controlPoints.add(new Point(posX+width,posY+height));
+		controlPoints.add(new Point(posX+width,posY));
+		Draw.curve(controlPoints, 0.0001, Color.green, TextureFactory.get("blank"));
+
+		//rednder a text
 		TrueTypeFont ttf = FontFactory.getFont("Times New Roman", Font.PLAIN,
 				fontSize);
 		ttf.drawString(posX, posY, "Hello world!",
@@ -67,5 +81,6 @@ public class HelloWorld extends GameTemplate {
 		}
 		
 	}
+	
 
 }
